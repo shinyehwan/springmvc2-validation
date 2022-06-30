c42f43a23a07e5768b1e66842b557cc8abb0fc56
shinyehwan <jghisjl007@naver.com>
2022-07-01 02:53:06 +0900

    검증2 - BeanVaildation Form 전송 객체 분리 - 개발까지

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,13 +1,28 @@
 package hello.itemservice.domain.item;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
 public class Item {
 
+//    @NotNull(groups = UpdateCheck.class)
     private Long id;
-    private String itemName;
-    private Integer price;
+
+//    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
+    private String itemName; // A
+
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Range(min = 1000, max=1000000, groups = {SaveCheck.class, UpdateCheck.class})
+    private Integer price; // A, typeMismatch
+
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Max(value = 9999, groups = SaveCheck.class)
     private Integer quantity;
 
     public Item() {
